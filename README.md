31d7d6b91c02c06315e96023ec0a4d8900ac25e0
Marcel Reis Soubkovsky <marcelclemente.msc@gmail.com>
2023-04-21 03:44:40 +0200

    feat: init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Local Comments
+
+This extension allows you to add comments to specific lines of code in a file, without modifying the code file itself. The comments are saved to a JSON file in your user root folder.
+
+## Features
+
+- Add comments to specific lines of code in a file.
+- Indicator on the line in the editor that it has a comment.
+- Input box to add, modify or delete comments.
+- Status bar button to trigger the input box.
+
+## Usage
+
+To add a comment to a specific line:
+
+1. Click on the line number on the left to select the line you want to add the comment to.
+2. Click on the status bar button to open the input box.
+3. Type your comment in the input box and press `Enter`.
+4. An indicator will appear on the line with your comment.
+
+To modify or delete a comment:
+
+1. Click on the line number on the left to select the line you want to modify or delete the comment for.
+2. Click on the status bar button to open the input box.
+3. Modify the existing comment or delete it by removing all the text from the input box and press `Enter`.
+4. The indicator on the line will be updated or removed accordingly.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of Local Comments.
+
+Enjoy!
+
+
---
+++ src/extension.ts
@@ -0,0 +1,117 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+interface CommentData {
+    [fileName: string]: {
+        [lineNumber: number]: string;
+    };
+}
+
+const commentData: CommentData = {};
+// save to user root folder
+const rootFolderPath = os.homedir();
+const commentFilePath = path.join(rootFolderPath, 'local-comments.json');
+
+function loadComments() {
+    if (fs.existsSync(commentFilePath)) {
+        const fileData = fs.readFileSync(commentFilePath, 'utf-8');
+        try {
+            Object.assign(commentData, JSON.parse(fileData));
+        } catch (err) {
+            console.error('Failed to parse comments file:', err);
+        }
+    }
+}
+
+function saveComments() {
+    try {
+        fs.writeFileSync(commentFilePath, JSON.stringify(commentData));
+        vscode.window.showInformationMessage(`Comments saved to ${commentFilePath}`);
+    } catch (err) {
+        console.error('Failed to save comments file:', err);
+    }
+}
+
+loadComments();
+
+const decorationType = vscode.window.createTextEditorDecorationType({
+    backgroundColor: 'rgba(255, 255, 0, 0.3)'
+});
+
+const statusBarBtn = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+statusBarBtn.text = "💭";
+statusBarBtn.tooltip = "Add a local comment";
+statusBarBtn.command = 'local-comments.openCommentInput';
+statusBarBtn.show();
+
+function updateDecorations() {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor) {
+        const decorations: vscode.DecorationOptions[] = [];
+        const fileName = activeEditor.document.fileName;
+        const commentsForFile = commentData[fileName];
+        if (commentsForFile) {
+            for (const [lineNumberStr, comment] of Object.entries(commentsForFile)) {
+                const lineNumber = parseInt(lineNumberStr);
+                const line = activeEditor.document.lineAt(lineNumber - 1);
+                const range = new vscode.Range(line.range.start, line.range.end);
+                const decoration = { range, hoverMessage: comment };
+                decorations.push(decoration);
+            }
+        }
+        activeEditor.setDecorations(decorationType, decorations);
+    }
+}
+
+updateDecorations();
+
+vscode.window.onDidChangeActiveTextEditor(updateDecorations);
+
+vscode.workspace.onDidChangeTextDocument((event) => {
+    if (event.document === vscode.window.activeTextEditor?.document) {
+        updateDecorations();
+    }
+});
+
+function openCommentInput() {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+        return;
+    }
+    const position = activeEditor.selection.active;
+    const lineNumber = position.line + 1;
+    const fileName = activeEditor.document.fileName;
+    const existingComment = commentData[fileName]?.[lineNumber] || '';
+    vscode.window.showInputBox({
+        value: existingComment,
+        prompt: `Comment for line ${lineNumber} in ${fileName}`
+    }).then((comment) => {
+        if (comment !== undefined) {
+            commentData[fileName] = commentData[fileName] || {};
+            commentData[fileName][lineNumber] = comment;
+            updateDecorations();
+            saveComments();
+            vscode.window.showInformationMessage(`Comment saved for line ${lineNumber} in ${fileName}`);
+        }
+    });
+}
+
+const disposable = vscode.commands.registerCommand('local-comments.openCommentInput', openCommentInput);
+
+vscode.window.onDidChangeVisibleTextEditors(updateDecorations);
+
+vscode.window.onDidChangeTextEditorSelection(updateDecorations);
+
+vscode.window.onDidChangeWindowState(updateDecorations);
+
+updateDecorations();
+
+export function activate(context: vscode.ExtensionContext) {
+    context. subscriptions.push(decorationType);
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(statusBarBtn);
+}
+
+export function deactivate() { }
